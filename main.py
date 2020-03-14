7207d22962eb4f7a51b94c0390d33de91f6abab5
stucklez <alexander.stuckler@gmail.com>
2020-03-14 11:55:24 +0100

    init

---
+++ main.py
@@ -0,0 +1,12 @@
+import pygame
+
+#Inittialize pygame
+pygame.init()
+#Creating screen
+screen = pygame.display.set_mode((800, 600))
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
